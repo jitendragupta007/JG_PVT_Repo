08dfc6f1434d527f99430f3e74d9b71de1330d4a
jitendragupta007 <itsjitendragupta007@gmail.com>
2023-08-10 13:11:41 +0530

    added new content

---
+++ handsomely-clam-5290/scripts/toggle.js
@@ -0,0 +1,28 @@
+
+
+
+
+let burger=document.querySelector(".hamburger");
+let slider=document.querySelector("#slider");
+let close=document.querySelector("#close");
+
+burger.addEventListener("click", function(){
+slider.style.display="block";
+});
+
+
+close.addEventListener("click", function(){
+    slider.style.display="none";
+    
+});
+
+
+
+
+
+
+
+
+
+
+
